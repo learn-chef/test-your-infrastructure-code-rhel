16c0d95fd9132da51443e2592f270495faa08676
tpetchel <tpetchel@gmail.com>
2015-10-05 15:38:37 -0400

    Serverspec tests

--- chef-repo/cookbooks/awesome_customers/test/integration/default/serverspec/default_spec.rb
+++ chef-repo/cookbooks/awesome_customers/test/integration/default/serverspec/default_spec.rb
@@ -1,12 +1,47 @@
 require 'spec_helper'
 
-describe 'awesome_customers::default' do
+describe 'the customers web application' do
+  it 'is up and running' do
+    expect(command('curl localhost').stdout).to match '<title>Customers</title>'
+  end
+end
 
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+describe 'the web_admin user' do
+  it 'has a login shell' do
+    expect(user 'web_admin').to have_login_shell '/bin/bash'
   end
 
+  it 'has a home directory' do
+    expect(user 'web_admin').to have_home_directory '/home/web_admin'
+  end
+end
+
+describe 'the default home page' do
+  it 'is owned by web_admin' do
+    expect(file '/var/www/customers/public_html/index.php').to be_owned_by 'web_admin'
+    expect(file '/var/www/customers/public_html/index.php').to be_grouped_into 'web_admin'
+  end
+end
+
+describe 'the default home page' do
+  let(:index) { '/var/www/customers/public_html/index.php' }
+  it 'is owned by web_admin' do
+    expect(file index).to be_owned_by 'web_admin'
+    expect(file index).to be_grouped_into 'web_admin'
+  end
+  it 'is a file' do
+    expect(file index).to be_file
+  end
+  it 'has mode 644' do
+    expect(file index).to be_mode 644
+  end
+end
+
+describe 'the httpd-customers service' do
+  it 'is enabled' do
+    expect(service 'httpd-customers').to be_enabled
+  end
+  it 'is running' do
+    expect(service 'httpd-customers').to be_running
+  end
 end
