0983da77d15f32f71255e29d4e161fd0097450ac
tpetchel <tpetchel@gmail.com>
2015-10-06 23:26:49 -0400

    webserver_test => webserver

--- webserver/spec/unit/recipes/default_spec.rb
+++ webserver/spec/unit/recipes/default_spec.rb
@@ -1,12 +1,12 @@
 #
-# Cookbook Name:: webserver_test
+# Cookbook Name:: webserver
 # Spec:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 require 'spec_helper'
 
-describe 'webserver_test::default' do
+describe 'webserver::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new
