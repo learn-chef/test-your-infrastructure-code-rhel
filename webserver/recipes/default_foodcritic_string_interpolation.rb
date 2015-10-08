cc0a0c6019581f6c3b6c06a0d0f37f39a0f4a111
tpetchel <tpetchel@gmail.com>
2015-10-08 15:09:58 -0400

    add additional examples as separate recipes

---
+++ webserver/recipes/default_foodcritic_string_interpolation.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: webserver
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+package_name = service_name = case node['platform']
+when 'centos' then 'httpd'
+when 'ubuntu' then 'apache2'
+end
+
+package "#{package_name}"
+
+service "#{service_name}" do
+  action [:enable, :start]
+end
+
+file '/var/www/html/index.html' do
+  content '<html>
+  <body>
+    <h1>hello world</h1>
+  </body>
+</html>'
+end
