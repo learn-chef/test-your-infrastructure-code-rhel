61711649e613bbe4b35d2f07fb78c48d068537e3
tpetchel <tpetchel@gmail.com>
2015-10-08 22:58:27 -0400

    refine tests to match tutorial

--- chef-repo/cookbooks/awesome_customers/test/integration/default/serverspec/default_spec.rb
+++ chef-repo/cookbooks/awesome_customers/test/integration/default/serverspec/default_spec.rb
@@ -7,12 +7,12 @@ describe 'the customers web application' do
 end
 
 describe 'the web_admin user' do
-  it 'has a login shell' do
-    expect(user 'web_admin').to have_login_shell '/bin/bash'
+  it 'has home directory /home/web_admin' do
+    expect(user 'web_admin').to have_home_directory '/home/web_admin'
   end
 
-  it 'has a home directory' do
-    expect(user 'web_admin').to have_home_directory '/home/web_admin'
+  it 'has login shell /bin/bash' do
+    expect(user 'web_admin').to have_login_shell '/bin/bash'
   end
 end
 
@@ -23,25 +23,24 @@ describe 'the default home page' do
   end
 end
 
-describe 'the default home page' do
-  let(:index) { '/var/www/customers/public_html/index.php' }
-  it 'is owned by web_admin' do
-    expect(file index).to be_owned_by 'web_admin'
-    expect(file index).to be_grouped_into 'web_admin'
-  end
-  it 'is a file' do
-    expect(file index).to be_file
-  end
-  it 'has mode 644' do
-    expect(file index).to be_mode 644
-  end
-end
-
 describe 'the httpd-customers service' do
   it 'is enabled' do
     expect(service 'httpd-customers').to be_enabled
   end
   it 'is running' do
     expect(service 'httpd-customers').to be_running
+  end
+end
+
+describe 'the default home page' do
+  let(:index) { '/var/www/customers/public_html/index.php' }
+  it 'exists' do
+    expect(file index).to exist
+  end
+  it 'is a file' do
+    expect(file index).to be_file
+  end
+  it 'has mode 644' do
+    expect(file index).to be_mode 644
   end
 end
